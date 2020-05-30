a5ae4be4c0f0cb0c7d29c2f103a78c5278f16180
Akash Munagala <akash.munagala@gmail.com>
2020-05-29 17:36:19 -0700

    Create imgui-memory-editor-sys

---
+++ imgui-memory-editor-sys/build.rs
@@ -0,0 +1,25 @@
+fn main() {
+    /*println!("cargo:rerun-if-changed=wrapper.h");
+
+    let bindings = bindgen::Builder::default()
+        .raw_line("#![allow(non_snake_case)]")
+        .raw_line("#![allow(non_upper_case_globals)]")
+        .raw_line("#![allow(dead_code)]")
+        .raw_line("#![allow(non_camel_case_types)]")
+        .header("wrapper.h")
+        .clang_arg("-x")
+        .clang_arg("c++")
+        .generate_inline_functions(true)
+        .whitelist_type("MemoryEditor")
+        .whitelist_function("MemoryEditor_.*")
+        .generate()
+        .expect("Unable to generate bindings");
+    bindings
+        .write_to_file("src/bindings.rs")
+        .expect("Couldn't write bindings!");*/
+    
+    cc::Build::new()
+    .cpp(true)
+    .file("wrapper.h")
+    .compile("imgui-memory-editor");
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,1 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+pub extern crate imgui_memory_editor_sys as sys;
